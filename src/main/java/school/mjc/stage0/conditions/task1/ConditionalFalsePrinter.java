01a09219721140629ac628d17bdb3e3496cbe57a
Vagharshak Samsonyan <vagosamsonyan@yahoo.com>
2024-03-31 15:03:14 +0400

    12 of 12 tests passed

--- src/main/java/school/mjc/stage0/conditions/task1/ConditionalFalsePrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task1/ConditionalFalsePrinter.java
@@ -2,5 +2,10 @@ package school.mjc.stage0.conditions.task1;
 
 public class ConditionalFalsePrinter {
     public static void main(String[] args) {
+        if (false) {
+            // Do nothing
+        } else {
+            System.out.println("false");
+        }
     }
 }
